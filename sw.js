3908419bf61d881b145657fb7062c423e2c659e7
Khushi Urkude <urkudesameer19@gmail.com>
2026-09-03 23:30:33 +0530

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,2 @@
+self.addEventListener('fetch', function(e) {});
+Once you do that, your app will automatically pass all PWA checks and users will be able to install it via your custom "Install App" button in the menu!
